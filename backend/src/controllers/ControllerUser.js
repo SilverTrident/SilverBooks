f78d596b7b390027e2c4c28a9676b7b7e83d3e42
Filipe Souza <souzasantos101@gmail.com>
2020-08-27 15:15:07 -0300

    hash password created

--- backend/src/controllers/ControllerUser.js
+++ backend/src/controllers/ControllerUser.js
@@ -1,15 +1,31 @@
 const ModelUser = require('../models/ModelUser');
+const bcrypt = require('bcryptjs');
+
 
 class ControllerUser {
     async created(req, res) {
         const user = new ModelUser(req.body);
-        await user.save()
-            .then(response => {
-                return res.status(200).json(response);
+
+        await bcrypt.genSalt(10, (erro, salt) => {
+            bcrypt.hash(user.password, salt, (erro, hash) => {
+
+                if (erro) {
+                    return res.status(400).json({ err: 'email já cadastrado' });
+                } else {
+                    user.password = hash;
+                    user.save()
+                        .then(response => {
+                            return res.status(200).json(response);
+                        })
+                        .catch(err => {
+                            return res.status(500).json(err);
+                        })
+                }
             })
-            .catch(err => {
-                return res.status(500).json(err);
-            })
+        })
+
+
+
     }
 }
 
