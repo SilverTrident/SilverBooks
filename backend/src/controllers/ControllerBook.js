c9e39283f952dd5cd5e48004c539d3c0ebc12ed9
Filipe Souza <souzasantos101@gmail.com>
2020-08-28 12:36:47 -0300

    created update and delete

--- backend/src/controllers/ControllerBook.js
+++ backend/src/controllers/ControllerBook.js
@@ -37,6 +37,44 @@ class ControllerBook {
             return res.status(500).json(err);
         });
     }
+    async listOne(req,res){
+        await ModelBook.findById(req.params.id).
+        then(response => {
+            if (response) {
+                return res.status(200).json(response);
+            } else {
+                return res.status(404).json({ err: 'book not found' });
+            }
+
+        }
+        ).catch(err => {
+            return res.status(500).json({ err: 'book not found'});
+        })
+    }
+    async update(req,res){
+        await ModelBook.findOneAndUpdate(
+            {'_id' : req.params.id},
+            req.body,
+            {new : true}).
+            then((response)=>{
+                return res.status(200).json(response);
+            }).catch(err => {
+                return res.status(500).json(err);
+            });
+    }
+    async delete(req, res) {
+        await ModelBook.deleteOne({ '_id': req.params.id }).
+            then(response => {
+                if (response) {
+                    return res.status(200).json(response);
+                } else {
+                    return res.status(404).json({ err: 'book not found' });
+                }
+            }
+            ).catch(err => {
+                return res.status(500).json({ err: 'book not found' });
+            })
+    }
 
 }
 
--- backend/src/routes/RouteBook.js
+++ backend/src/routes/RouteBook.js
@@ -2,11 +2,16 @@ const ControllerBook = require('../controllers/ControllerBook');
 const MiddlewareBook = require('../Middlewares/MiddlewareBook')
 const MiddlewareAuthenticated = require('../Middlewares/MiddlewareAuthenticated')
 const express = require('express');
+const { Router } = require('express');
 
 const route = express.Router();
 
 route.post('/admin',MiddlewareAuthenticated.admin,MiddlewareBook,ControllerBook.created);
-route.get('/admin',MiddlewareAuthenticated.admin,ControllerBook.listAll);
+route.delete('/admin/:id',MiddlewareAuthenticated.admin, ControllerBook.delete)
+route.put('/admin/:id',MiddlewareAuthenticated.admin,ControllerBook.update)
+route.get('/admin/edit/:id',MiddlewareAuthenticated.admin,ControllerBook.listOne)
+route.get('/admin/listall',MiddlewareAuthenticated.admin,ControllerBook.listAll);
+
 route.get('/',ControllerBook.listAll);
 route.get('/:category',ControllerBook.listCategory);
 
