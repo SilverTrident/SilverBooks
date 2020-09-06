04a5f93180853b8c9149520938d6bd7cb5e4a7fa
Filipe Souza <souzasantos101@gmail.com>
2020-09-06 18:27:09 -0300

    initialized frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './assets/styles/global.css'
+import HomePage from './pages/HomePage'
+function App() {
+    return ( 
+    <HomePage/>
+    );
+}
+
+export default App;
---
+++ frontend/src/pages/HomePage/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HomePage(){
+    return(
+        <div id="home-page">
+            <div id="home-page-content" className="container">
+                
+            </div>
+        </div>
+    
+    );
+}
+
+export default HomePage;
