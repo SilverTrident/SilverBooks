59b235a6f45c27f95b7ec76c6b5698f174b0b0ce
Filipe Souza <souzasantos101@gmail.com>
2020-09-11 19:39:42 -0300

    aler lock-json

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,12 @@ const RouteBook = require('./routes/RouteBook');
 const RouteUser = require('./routes/RoutesUser')
 const express =require('express');
 const session =require('express-session');
+const cors = require('cors')
 const server = express();
 
 
 //configs
+server.use(cors());
 server.use(express.json());
 server.use(session({
     secret : '2fe619988a70829f1b6cb455cd493905',
@@ -20,5 +22,5 @@ server.use('/user',RouteUser);
 
 
 
-server.listen('3000');
+server.listen('3001');
 
